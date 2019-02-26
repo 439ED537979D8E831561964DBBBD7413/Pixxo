0b8bac4644ed3b88ecfdf97c51906143d4a5e05c
breezil <kolajubril@gmail.com>
2019-02-26 10:12:36 -0500

    Fix: change library package name for photo editor

--- photoeditor/src/main/java/com/example/photoeditor/filters/FilterListener.java
+++
@@ -1,7 +0,0 @@
-package com.pixxo.photoeditor.filters;
-
-import ja.burhanrashid52.photoeditor.PhotoFilter;
-
-public interface FilterListener {
-    void onFilterSelected(PhotoFilter photoFilter);
-}
